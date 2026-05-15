21af86cd73b1d4acbe68b9437a1e84d30fee36d8
Devarshi_Jani <janidevarshi8@gmail.com>
2026-05-15 12:53:00 +0530

    fix deployment

--- task-management-backend/server.js
+++ task-management-backend/server.js
@@ -5,7 +5,7 @@ const app = require("./src/app");
 const connectDB = require("./src/config/db");
 const initSocket = require("./src/sockets/socket");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const startServer = async () => {
     await connectDB();
